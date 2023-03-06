7806e709afdf9c2537b9afb41fb26582cbe84941
zannujulius <zannujulius14@gmail.com>
2023-03-06 07:37:44 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect } from "react";
+import axios from "axios";
 function App() {
+  useEffect(() => {
+    (async () => {
+      try {
+        let res = await axios.get("https://medium.com/feed/@remisafrica");
+        console.log(res.data);
+      } catch (err) {
+        console.log(err.message);
+      }
+    })();
+    return () => {};
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="">Blog post</div>
     </div>
   );
 }
